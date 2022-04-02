db72860e5165e6dbef3835bee6b6b800c326c0d0
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-04-02 08:09:40 -0600

    Implement Bootstrap

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,16 +1,24 @@
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Container from 'react-bootstrap/Container'
+
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
+    const displayPackages = props.packages.map((eachPackage) => 
+    <ListGroup.Item>{eachPackage}</ListGroup.Item>)
 
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
-                    {displayPackages}
-                </ul>
-            </div>
-        </div>
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Our Packages</Card.Title>
+                    <Card.Text>
+                        Check out some of our packages! Every package is ethically sourced and organic!
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup>
+                    {displayPackages} 
+                </ListGroup>
+            </Card>
+        </Container>
     )
 }
